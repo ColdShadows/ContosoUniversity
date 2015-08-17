99fb04a440ba8f1469ac4f8428f76934cc3c5aef
Travis K <traviskean04@live.com>
2015-08-17 16:45:15 -0400

    Created Stored Procedures

--- ContosoUniversity/Controllers/CourseController.cs
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -126,7 +126,20 @@ namespace ContosoUniversity.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewBag.RowsAffected = db.Database.ExecuteSqlCommand("UPDATE Course SET Credits = Credits * {0}", multiplier);
+            }
+            return View();
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
